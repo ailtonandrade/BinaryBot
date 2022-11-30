648ebfa141aa1a14c78a61d4fae50209868204f5
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Login from "./views/login/Login.vue";
+</script>
+
+<template>
+  <Login />
+</template>
+
+<style scoped>
+@import url('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <form>
+      <div class="mb-3">
+        <label for="usuario" class="form-label">Usuário:</label>
+        <input type="text" class="form-control" id="name" v-model="usuario" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Senha:</label>
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          v-model="password"
+        />
+      </div>
+      <div class="mb-3">
+        <button type="button" class="btn btn-primary" @click="methods.login()">
+          Entrar
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import authService from '../../services/authService.ts';
+
+export default {
+  setup() {
+    const title = ref("BinaryBot");
+    const description = ref(
+  "Automatize suas operações binårias e trades na plataforma mais usada no mundo"
+    );
+    const usuario = ref("");
+    const password = ref("");
+
+    const methods = {
+      login() {
+        if (!usuario.value || !password.value) {
+          alert("Por favor, preencha todos os campos.");
+        } else {
+          console.log("Efetuando login");
+          authService.login(usuario.value,password.value);
+       }
+      },
+    };
+
+    return {
+      title,
+      description,
+      usuario,
+      password,
+      methods,
+    };
+  },
+};
+</script>
