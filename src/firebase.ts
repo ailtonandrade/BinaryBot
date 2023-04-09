751daa4b8e483456f892c0bebf63a49ad3944277
Ailton Andrade Tom <ailtonandrade15@gmail.com>
2023-04-09 19:30:33 +0000

    add firestore

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,35 +1,10 @@
-import axios from "axios";
+import { db } from "../firebase";
 
-const authService = {
-  login: async function (username: string, password: string) {
-    try {
-      console.log("Enviando dados.");
-      const response = await axios.post("/api/login", { username, password });
-      const token = response.data.token;
-      localStorage.setItem("token", token);
-      return token;
-    } catch (error) {
-    alert("Erro ao autenticar : "+error.response.data);
-      throw new Error(error.response.data.message || "Erro ao efetuar login.");
-    }
-  },
-
-  logout: function () {
-    localStorage.removeItem("token");
-  },
-
-  isAuthenticated: function () {
-    const token = localStorage.getItem("token");
-    return token !== null && token !== undefined;
-  },
-
-  getToken: function () {
-    const token = localStorage.getItem("token");
-    if (token) {
-      return token;
-    }
-    throw new Error("Token não encontrado.");
-  },
-};
-
-export default authService;
+async function login(user: string, pass: string) {
+  try {
+    const doc = await db.collection("users").get();
+    alert(doc);
+  } catch (ex) {
+    alert("Erro ao efetuar Login" + ex);
+  }
+}
